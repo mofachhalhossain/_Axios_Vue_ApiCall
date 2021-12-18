704e3450ae53517d688d09b523e648bbd83a1119
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-12-18 14:37:29 +0600

    api fetched

---
+++ src/components/CallApi.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Api Call</h1>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "CallApi",
+  created() {
+    axios
+      .get("https://api.github.com/users")
+      .then((response) => {
+        this.user = response.data;
+        console.log(response);
+        console.log(this.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style scoped></style>
